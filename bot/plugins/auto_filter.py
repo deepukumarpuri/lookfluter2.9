2c0f7cd8c3266a18a813e2a9c31eb6c04c2a4849
anandhu <84675486+Anandhu123555@users.noreply.github.com>
2021-07-25 11:26:11 +0100

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -209,7 +209,7 @@ async def auto_filter(bot, update):
             await bot.send_photo(
                 chat_id = update.chat.id,
                 photo="https://telegra.ph/file/18bfb522051079bc3428b.jpg",
-                caption=f"<b>𝐆𝐫𝐨𝐮𝐩:- @Cinemahub182</b>...\n𝐑𝐞𝐪𝐮𝐞𝐬𝐭𝐞𝐝 𝐁𝐲:- {update.from_user.mention} 😏\n 𝐑𝐞𝐪𝐮𝐞𝐬𝐭𝐞𝐝 𝐌𝐨𝐯𝐢𝐞:- <code>{query}</code> 👩‍💻\n 𝐓𝐨𝐭𝐚𝐥 𝐑𝐞𝐬𝐮𝐥𝐭𝐬:- {(len_results)} 💯\n 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 :- @Leos_Tg ⚜",
+                caption=f"<b>𝐆𝐫𝐨𝐮𝐩:- @Cinemahub182</b>...\n𝐑𝐞𝐪𝐮𝐞𝐬𝐭𝐞𝐝 𝐁𝐲:- {update.from_user.mention} 😏\n𝐑𝐞𝐪𝐮𝐞𝐬𝐭𝐞𝐝 𝐌𝐨𝐯𝐢𝐞:- <code>{query}</code> 👩‍💻\n𝐓𝐨𝐭𝐚𝐥 𝐑𝐞𝐬𝐮𝐥𝐭𝐬:- {(len_results)} 💯\n𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 :- @Leos_Tg ⚜",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
